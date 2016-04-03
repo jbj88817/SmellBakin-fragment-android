0f3420ea45ebc8f97a00d4689b048de2afb376a3
Bojie Jiang <jiangbojie@gmail.com>
2016-04-03 10:46:24 -0700

    first commit

---
+++ app/src/main/java/com/bojie/smellslikebakin/ListFragment.java
@@ -0,0 +1,22 @@
+package com.bojie.smellslikebakin;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by bojiejiang on 4/3/16.
+ */
+public class ListFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_list, container);
+        return view;
+    }
+}
